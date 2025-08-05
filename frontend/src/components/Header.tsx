cc0d59218cc93b7ed1d4e75a3dee4fba1f24d30a
Seikixo <150346850+Seikixo@users.noreply.github.com>
2025-08-06 06:53:45 +0800

    chore: refactor header into component

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,63 @@
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useAuth } from '../hooks/useAuth';
+import {
+  Navbar,
+  NavbarCollapse,
+  NavbarLink,
+  NavbarToggle,
+} from 'flowbite-react';
+
+export default function Header() {
+  const { logout } = useAuth();
+  const location = useLocation();
+  const navigate = useNavigate();
+
+
+    return(
+        <>
+        <Navbar fluid className="bg-blue-600 text-white">
+            <Link
+            to="/home"
+            className="text-white text-xl font-semibold px-4 py-2 flex items-center"
+            >
+            My App
+            </Link>
+            <NavbarToggle />
+            <NavbarCollapse>
+                <NavbarLink 
+                    href="/home" 
+                    active={location.pathname === '/home'}
+                    className="text-white"
+                    onClick={(e) => {
+                    e.preventDefault();
+                    navigate('/home');
+                    }}
+                >
+                    Home
+                </NavbarLink>
+
+                <NavbarLink 
+                    href="/blog" 
+                    active={location.pathname === '/blog'}
+                    className="text-white"
+                    onClick={(e) => {
+                    e.preventDefault();
+                    navigate('/blog');
+                    }}
+                >
+                    Blog
+                </NavbarLink>
+                <NavbarLink 
+                    className="text-white cursor-pointer"
+                    onClick={(e) => {
+                    e.preventDefault();
+                    logout();
+                    }}
+                >
+                    Logout
+                </NavbarLink>
+            </NavbarCollapse>
+        </Navbar>        
+        </>
+    )
+}
--- frontend/src/routes/ProtectedRoute.tsx
+++ frontend/src/routes/ProtectedRoute.tsx
@@ -1,65 +1,18 @@
-import { Navigate, Outlet, Link, useLocation, useNavigate } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
-import {
-  Navbar,
-  NavbarCollapse,
-  NavbarLink,
-  NavbarToggle,
-} from 'flowbite-react';
+
+import Header from '../components/Header';
 
 
 export default function ProtectedRoute() {
-  const { user, isLoading, logout } = useAuth();
-  const location = useLocation();
-  const navigate = useNavigate();
+  const { user, isLoading} = useAuth();
+
 
   if (isLoading) return <div><p>Loading...</p></div>
   if (!user) return <Navigate to="/login" replace />;
   return (
     <div className="min-h-screen flex flex-col">
-      <Navbar fluid className="bg-blue-600 text-white">
-        <Link
-          to="/home"
-          className="text-white text-xl font-semibold px-4 py-2 flex items-center"
-        >
-          My App
-        </Link>
-        <NavbarToggle />
-        <NavbarCollapse>
-          <NavbarLink 
-            href="/home" 
-            active={location.pathname === '/home'}
-            className="text-white"
-            onClick={(e) => {
-              e.preventDefault();
-              navigate('/home');
-            }}
-          >
-            Home
-          </NavbarLink>
-
-          <NavbarLink 
-            href="/blog" 
-            active={location.pathname === '/blog'}
-            className="text-white"
-            onClick={(e) => {
-              e.preventDefault();
-              navigate('/blog');
-            }}
-          >
-            Blog
-          </NavbarLink>
-          <NavbarLink 
-            className="text-white cursor-pointer"
-            onClick={(e) => {
-              e.preventDefault();
-              logout();
-            }}
-          >
-            Logout
-          </NavbarLink>
-        </NavbarCollapse>
-      </Navbar>
+      <Header/>
 
       <main className="flex-1 p-6">
         <Outlet />
