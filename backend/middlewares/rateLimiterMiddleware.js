a1ce02f2703ddf562215e16fdc69040629a451a7
Seikixo <150346850+Seikixo@users.noreply.github.com>
2025-08-05 17:53:21 +0800

    chore: rate limiting in post

--- backend/middlewares/rateLimiterMiddleware.js
+++ backend/middlewares/rateLimiterMiddleware.js
@@ -1,10 +1,17 @@
 const rateLimit = require('express-rate-limit');
 
 const reactionLimiter = rateLimit({
-  windowMs: 60 * 1000, 
+  windowMs: 120 * 1000, 
   max: 5,             
   message: 'Too many reactions. Please wait and try again later.',
   keyGenerator: (req) => req.user.id, 
 });
 
-module.exports = { reactionLimiter }
+const postLimiter = rateLimit({
+  windowMs: 300 * 1000,
+  max: 3,
+  message: 'Too many post. Please wait and try again later.',
+  keyGenerator: (req) => req.user.id,
+})
+
+module.exports = { reactionLimiter, postLimiter }
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { protect } = require('../middlewares/authMiddleware');
 const { getAllPosts, updatePost, deletePost, createPost, likePost, dislikePost } = require('../controllers/postController');
-const { reactionLimiter } = require('../middlewares/rateLimiterMiddleware');
+const { reactionLimiter, postLimiter } = require('../middlewares/rateLimiterMiddleware');
 
-router.route('/').get(getAllPosts).post(protect, createPost);
+router.route('/').get(getAllPosts).post(protect, postLimiter, createPost);
 router.route('/:id').put(protect, updatePost).delete(protect, deletePost);
 
 router.post('/:id/like', protect, reactionLimiter, likePost);
