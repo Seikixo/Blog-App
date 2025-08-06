60ece808ac9734f643f1a09277b565359ac95a39
Seikixo <150346850+Seikixo@users.noreply.github.com>
2025-08-06 09:26:52 +0800

    fix: add get user posts in routes

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../middlewares/authMiddleware');
-const { getAllPosts, updatePost, deletePost, createPost, likePost, dislikePost } = require('../controllers/postController');
+const { getAllPosts, getUserPosts, updatePost, deletePost, createPost, likePost, dislikePost } = require('../controllers/postController');
 const { reactionLimiter, postLimiter } = require('../middlewares/rateLimiterMiddleware');
 
-router.route('/').get(getAllPosts).post(protect, postLimiter, createPost);
-router.route('/:id').put(protect, updatePost).delete(protect, deletePost);
+
+router.get('/', getAllPosts);
+
+router.get('/me', protect, getUserPosts);
+
+router.post('/', protect, postLimiter, createPost);
+
+router.route('/:id')
+  .put(protect, updatePost)
+  .delete(protect, deletePost);
+
 
 router.post('/:id/like', protect, reactionLimiter, likePost);
 router.post('/:id/dislike', protect, reactionLimiter, dislikePost);
