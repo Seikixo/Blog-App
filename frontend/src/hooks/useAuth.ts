f1f44bdfe79068d695fb3f5b2c48e54e8870d3ce
Seikixo <150346850+Seikixo@users.noreply.github.com>
2025-08-09 20:13:31 +0800

    fix: token initialization

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -4,17 +4,15 @@ import { login as loginApi, register as registerApi } from "../services/authServ
 import { getUserProfile } from "../services/userService";
 import { useNavigate } from "react-router-dom";
 import type { Credentials, RegisterUserPayload } from "../types/types";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 
 export const useAuth = () => {
     const queryClient = useQueryClient();
-    const [token, setToken] = useState<string | null>(null);
+    const [token, setToken] = useState<string | null>(
+        () => localStorage.getItem("token")
+    );
 
-    useEffect(() => {
-        const storedToken = localStorage.getItem("token");
-        setToken(storedToken);
-    }, []);
     const navigate = useNavigate();
 
     const {data: user, isLoading} = useQuery({
