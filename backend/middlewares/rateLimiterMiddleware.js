d2ddcec8b02a4e1cedd964a178981c983248f33f
Seikixo <150346850+Seikixo@users.noreply.github.com>
2025-08-12 15:46:53 +0800

    chore: update rate limiting to likes and dislikes

--- backend/middlewares/rateLimiterMiddleware.js
+++ backend/middlewares/rateLimiterMiddleware.js
@@ -1,15 +1,15 @@
 const rateLimit = require('express-rate-limit');
 
 const reactionLimiter = rateLimit({
-  windowMs: 120 * 1000, 
-  max: 5,             
+  windowMs: 300 * 1000, 
+  max: 30,             
   message: 'Too many reactions. Please wait and try again later.',
   keyGenerator: (req) => req.user.id, 
 });
 
 const postLimiter = rateLimit({
   windowMs: 300 * 1000,
-  max: 3,
+  max: 5,
   message: 'Too many post. Please wait and try again later.',
   keyGenerator: (req) => req.user.id,
 })
